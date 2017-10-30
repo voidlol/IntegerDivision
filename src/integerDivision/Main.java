087a100f855b9926a8713ce97e1988cc28ff70a3
Savelov Andrey <udroguedozed@gmail.com>
2017-10-31 00:08:33 +0300

    test

--- src/integerDivision/Main.java
+++ src/integerDivision/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		LongDivision division = new LongDivision();
-		DivisionData divisionData = division.divide(100, 3);
+		DivisionData divisionData = division.divide(25, 39);
 		DivisionFormatter divisionFormatter = new DivisionFormatter();
 		System.out.println(divisionFormatter.formatData(divisionData));
 	}
